371a737ea8797932248e63acec936b292e49f560
Hryhorii Skobiennikov <juskobe@gmail.com>
2022-05-20 00:57:08 +0300

    lesson info

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+a = "hello"
+b = 25
+
+
+print(a)
+print (b)
+
+#filepath = input()
+#file = open(filepath, 'r')
+
+#print (file)
+
+for i in range(0,-100,-2):  
+    print(i)
